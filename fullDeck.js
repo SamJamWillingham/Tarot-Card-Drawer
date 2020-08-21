c22553c02db46a61c61d81e445c1bf5e86c3a635
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2020-08-21 12:55:34 -0400

    added images to src file, added to fullDeck array, added js to randomally select a card

---
+++ fullDeck.js
@@ -0,0 +1,78 @@
+let deck = [
+  {
+    name: "The Fool",
+    cardFile: `<img src="src/0-fool.jpg" alt="The Fool" class="cardImg">`,
+    about:
+      "The Fool advises that you lighten up. Let yourself be spontaneous enough to stretch beyond the realm of logic. There is no advantage to be gained by thinking you possess the knowledge, power, or control to direct reality. Open and receive without question, instead of trying to manage what 's happening right now. The Fool has no ambition to manipulate a specific outcome. Just be happy to be part of the whole.",
+  },
+
+  {
+    name: "The Magician",
+    cardFile: `<img src="src/1-magician.jpg" alt="The Magician" class="cardImg">`,
+    about: `Have faith in your innate creativity. The Magician advises you give your questioning nature and free-associating mind plenty of room to explore the subject at hand. Behave just as if you were an open-minded and curious scientist. Through this process, you may bring freshness and clarity into the situation that is both stimulating and catalytic. <br />You do not have to understand it all intellectually. Besides, intuition is your ace in the hole. Respond in a spontaneous manner to what is right in front of you. There is no reason to hold yourself back.`,
+  },
+
+  {
+    name: "The High Priestess",
+    cardFile: `<img src="src/2-high-priestess.jpg" alt="The High Priestess" class="cardImg">`,
+    about: `The High Priestess advises you to adhere to your chosen spiritual practice on a more regular basis. If you want the benefits of evolution, you\'ll have to cooperate with spirit. We all have distractions, demands -- a whole life full of reasons why we cannot find the time to retreat into our inner sanctum. A spiritual routine that suits your temperament, practiced every day, is the most trustworthy path to freedom.`,
+  },
+
+  {
+    name: "The Empress",
+    cardFile: `<img src="src/3-empress.jpg" alt="The Empress" class="cardImg">`,
+    about: `Demonstrate this through caring actions, a forgiving and generous attitude, and wise understanding of others' needs and struggles. Bring a healing influence to the current situation and take full credit for the supportive part you play.`,
+  },
+
+  {
+    name: "The Emperor",
+    cardFile: `<img src="src/4-emperor.jpg" alt="The Emperor" class="cardImg">`,
+    about: `Reach within to find that strain of natural nobility and leadership. These innate qualities will help you manage your current situation.<br />The Emperor suggests that you have the necessary abilities to be the final authority. This situation is an opportunity to showcase your competence and skill. Act with the confidence of someone who knows how to take care of business.`,
+  },
+
+  {
+    name: "The Hierophant",
+    cardFile: `<img src="src/5-hierophant.jpg" alt="The Hierophant" class="cardImg">`,
+    about: `Learn everything you can about your chosen area. Let that knowledge become a part of you and an operative influence on your day-to-day awareness. In this way, you can slowly and steadily establish real credibility in your field or chosen subject.<br />Focus on your goal and be determined. You may be destined to be a master in your realm.`,
+  },
+
+  {
+    name: "The Lovers",
+    cardFile: `<img src="src/6-lovers.jpg" alt="The Lovers" class="cardImg">`,
+    about: `The Lovers card advises that you study your options and make the wisest choice. Carefully consider your long-term interests. There is no judgment on what you choose to keep from the array of possibilities before you. Just watch out for choices that will produce dissatisfaction and discontent.<br />Be willing to make some compromises, then stick with the commitments you finally make.`,
+  },
+
+  {
+    name: "The Chariot",
+    cardFile: `<img src="src/7-chariot.jpg" alt="The Chariot" class="cardImg">`,
+    about: `The Chariot advises that you be prepared for changes that might include a move or an opportunity to travel. The Charioteer travels light and stays open to fresh experiences that change with every valley or mountain pass.<br />Be receptive to new people who come into your life. Most especially, become more fluid and taste the joys of freedom. The Charioteer is nothing if not self-sufficient.`,
+  },
+  {
+    name: "Strength",
+    cardFile: `<img src="src/8-strength.jpg" alt="Strength" class="cardImg">`,
+    about: `The Strength card advises that you assertively discipline yourself and separate self interest from enlightened wisdom. Deliberately identify with your intuition, even if it works against the desires of your willful ego.<br />Demand and expect the same from others who have some power in this situation. You cannot challenge them to live to a higher standard if you, yourself, have not yet done so. Influence others by setting an example of integrity. Your self-esteem will increase to the degree that you succeed in your efforts.`,
+  },
+
+  {
+    name: "The Hermit",
+    cardFile: `<img src="src/9-hermit.jpg" alt="The Hermit" class="cardImg">`,
+    about: `The Hermit advises that you think things through carefully. The demands on you have been high, giving you scant time for reflection. While you have a gift for understanding the larger implications involved, you need some private time to consider the steps to take in the future. You can't just lock yourself in your room for fifteen minutes and expect to come up with profound solutions. You need more seclusion and time to assimilate and process.<br />Now may be the moment for you to tell everyone to leave you alone. When you are fully ready, you will be able and willing to give others what they need.`,
+  },
+  {
+    name: "The Wheel of Fortune",
+    cardFile: `<img src="src/10-wheel.jpg" alt="The Wheel of Fortune" class="cardImg">`,
+    about: `The Wheel of Fortune advises you follow the flow of events. Physical moves, spiritual awakenings, or dramatically changing social patterns could arise now. Accept these transformations.<br />This is a safe place for you to be. You are watched over and protected as you go round and round the wheel. You will learn a lot. You will also learn it quickly, and what you absorb will benefit you for a long time to come.`,
+  },
+
+  {
+    name: "Justice",
+    cardFile: `<img src="src/11-justice.jpg" alt="Justice" class="cardImg">`,
+    about: `The Justice card advises you to listen carefully as others explain to you their version of events and the parts they played. It is unnecessary to offer feedback. Your role is to observe, listen closely, give a full hearing and keep your wits about you as the story takes shape.<br />As you witness people's account of themselves, your understanding will go beyond the words you hear.`,
+  },
+
+  {
+    name: "The Hanged Man",
+    cardFile: `<img src="src/12-hanged-man.jpg" alt="The Hanged Man" class="cardImg">`,
+    about: `The Hanged Man advises you to surrender illusions of control. Accept that you have been overcome and deceived by your own devices. This is not a negative judgment. It's just that sometimes there are consequences for being in the wrong place at the wrong time. These consequences are not fatal, even if they are inconvenient -- or perhaps even embarrassing.<br />Stop resisting your circumstances and let some time go by. Eventually, you will be released a little wiser and not much the worse for wear. You will come to realize in time how you collaborated with the problem. However, the issues you were stuck on when you were first hung up have subsided and no longer concern you. You are free to take up new endeavors. You will ultimately feel refreshed and grateful that you were derailed from your former track.`,
+  },
+];
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let card = document.querySelector("div.card");
+let cardName = document.querySelector("h3.name");
+let moreInfo = document.querySelector("p.about");
 
-function changeCard(event) {
-  event.preventDefault();
-  card.innerHTML = `<img src="src/0-fool.jpg" alt="The Fool" class="cardImg">`;
+function changeCard() {
+  let index = getRandomNumber(0, deck.length - 1);
+  card.innerHTML = deck[index].cardFile;
+  cardName.innerHTML = deck[index].name;
+  moreInfo.innerHTML = deck[index].about;
+
   card.classList.remove("default-styling");
+  card.classList.add("newCard");
+}
+
+function getRandomNumber(min, max) {
+  let step1 = max - min + 1;
+  let step2 = Math.random() * step1;
+  let result = Math.floor(step2) + min;
+
+  return result;
 }
 
 card.addEventListener("click", changeCard);
